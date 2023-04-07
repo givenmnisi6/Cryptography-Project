f5acdb98868b5e20f3c658ae82b6683aa3de93ba
GIVEN MNISI <givenmnisi6@gmail.com>
2023-04-07 18:58:38 +0200

    Text Encryption

--- Cryptography_Project/TextForm.cs
+++ Cryptography_Project/TextForm.cs
@@ -1,5 +1,6 @@
 ﻿using Cryptography_Project.Custom;
 using Cryptography_Project.Transposition;
+using Cryptography_Project.Vernam;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -64,8 +65,8 @@ namespace Cryptography_Project
 
                         if (int.TryParse(encryptionTextbox.Text, out test))
                         {
-                            TranspositionText trans = new TranspositionText(keyValue = Convert.ToInt32(encryptionTextbox.Text));
-                            cipherTextbox.Text = trans.Encrypt(plainText);
+                            TranspositionText transposition = new TranspositionText(keyValue = Convert.ToInt32(encryptionTextbox.Text));
+                            cipherTextbox.Text = transposition.Encrypt(plainText);
                         }
                         MessageBox.Show("Sucessfully encrypted the text with the transposition algorithm!");
                     }
@@ -75,8 +76,8 @@ namespace Cryptography_Project
 
                         if (int.TryParse(encryptionTextbox.Text, out test))
                         {
-                            TranspositionText trans = new TranspositionText(keyValue = Convert.ToInt32(encryptionTextbox.Text));
-                            cipherTextbox.Text = trans.Decrypt(plainText);
+                            TranspositionText transposition = new TranspositionText(keyValue = Convert.ToInt32(encryptionTextbox.Text));
+                            cipherTextbox.Text = transposition.Decrypt(plainText);
                         }
                         MessageBox.Show("Sucessfully decrypted the text with the transposition algorithm!");
                     }
@@ -85,23 +86,40 @@ namespace Cryptography_Project
                 {
                     if (encryptionRadiobtn.Checked)
                     {
-                        CustomText customText = new CustomText();
-                        cipherTextbox.Text = customText.CustomTextEncryption(plainText, encryptionKey);
+                        CustomText custom = new CustomText();
+                        cipherTextbox.Text = custom.CustomTextEncryption(plainText, encryptionKey);
                         MessageBox.Show("Sucessfully encrypted the text with the B&G InHouse algorithm!");
                     }
                     else if (decryptionRadiobtn.Checked)
                     {
-                        CustomText customText = new CustomText();
-                        cipherTextbox.Text = customText.CustomTextDecryption(plainText, encryptionKey);
+                        CustomText custom = new CustomText();
+                        cipherTextbox.Text = custom.CustomTextDecryption(plainText, encryptionKey);
                         MessageBox.Show("Sucessfully encrypted the text with the B&G InHouse algorithm!");
+                    }
+                } 
+                else if (comboBox1.SelectedItem.ToString() == "Vernam")
+                {
+                    if (encryptionRadiobtn.Checked)
+                    {
+                        VernamText vernam = new VernamText();
+                        cipherTextbox.Text = vernam.VernamEncryptionAndDecryption(plainText, encryptionKey);
+                        MessageBox.Show("Sucessfully encrypted the text with the Vernam algorithm!");
+                    }
+                    else if (decryptionRadiobtn.Checked)
+                    {
+                        VernamText vernam = new VernamText();
+                        cipherTextbox.Text = vernam.VernamEncryptionAndDecryption(plainText, encryptionKey);
+                        MessageBox.Show("Sucessfully decrypted the text with the Vernam algorithm!");
                     }
                 }
             }
             else
             {
-                MessageBox.Show("You need to input text and a numerical key");
+                MessageBox.Show("Please check your inputs");
             }
         }
+
+
         private void Clearbtn_Click(object sender, EventArgs e)
         {
             plainTextbox.Text = "";
---
+++ Cryptography_Project/Vernam/VernamText.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography_Project.Vernam
+{
+    internal class VernamText
+    {
+        //Uses a simple XOR-based encryption algorithm
+        public string VernamEncryptionAndDecryption(string plainText, string key)
+        {
+            var encryptedText = new StringBuilder();
+
+            for (int cipherText = 0; cipherText < plainText.Length; cipherText++)
+            {
+                //get the current character from the plainText & take next character from string
+                char character = plainText[cipherText];
+
+                //cast the character to a 32-bit unsigned interger.
+                uint charCode = (uint)character;
+
+                // calculate the index of the key character for the plaintext
+                // this line his ensures that the key is used in a repeating
+                // pattern to encrypt or decrypt the plaintext
+                int keyPosition = cipherText % key.Length;
+
+                //take the key character at the calculated index
+                char keyChar = key[keyPosition];
+
+                //cast the key character to a 32-bit unsigned integer
+                uint keyCode = (uint)keyChar;
+
+                // perform XOR on the two character codes to produce a new 32-bit unsigned character
+                uint combinedCode = charCode ^ keyCode;
+
+                //cast the combined code back to a char
+                char combinedChar = (char)combinedCode;
+
+                //Append the combined character 
+                encryptedText.Append(combinedChar);
+            }
+            //the StringBuilder contains the encrypted or decrypted text
+            return encryptedText.ToString();
+        }
+    }
+}
+
